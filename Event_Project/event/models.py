9afac556cd7955268865d9336a67b51e8dbdca7f
Jetli19 <andrej.lukacka@gmail.com>
2022-10-03 21:19:45 +0200

    update main,navbar, home

--- Event_Project/event/models.py
+++ Event_Project/event/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+
+class Event(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = models.ManyToManyField(
+    #     User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
--- Event_Project/event/views.py
+++ Event_Project/event/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from django.forms import ModelForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
 
-# Create your views here.
+from event.models import Event
+
+
+def home(request):
+    events = Event.objects.all()  # najdeme všechny místnosti
+
+    context = {'events': events}
+    return render(request, 'event/home.html', context)
